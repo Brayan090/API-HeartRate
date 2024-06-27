26b655fde01c3f0cb1e0f15488e869397f492d36
Brayan090 <86983284+Brayan090@users.noreply.github.com>
2024-06-27 14:03:27 -0600

    Heart-Rate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/artist',pathMatch:'full'},
+  {path:'artist',loadChildren:()=>import('./Componenets/content/content.module').then(m=>m.ContentModule)},
+  {path:'auth',loadChildren:()=>import('./Componenets/auth/auth.module').then(m=>m.AuthModule)},
+  {path:'library',loadChildren:()=>import('./Componenets/library/library.module').then(m=>m.LibraryModule)}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
